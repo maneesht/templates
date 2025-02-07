e1329d33d323661052c32b93f44a611dbec1c736
Maneesh Tewani <mtewani@google.com>
2025-02-07 13:59:40 -0800

    Removed movie app

--- data-connect/flutter-movie/android/app/src/main/kotlin/com/example/dataconnect/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dataconnect
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- data-connect/idx-template.nix
+++ data-connect/idx-template.nix
@@ -32,8 +32,6 @@ idx-template \
     cp -r data_connect \"$out\"
     " else "
     mkdir -p \"$out\"/.idx
-    chmod -R u+w \"$out\"
-    chmod -R u+w \"$out\"/.idx
     "}
     chmod -R u+w "$out"
     ${if appType == "quickstart" then "
